5f52da8e5cfa4c87cbc00a01ac756ee945aad4ac
xTheM4ST3Rx <them4sterdeveloper@outlook.com>
2022-03-29 10:12:43 -0300

    update nestjs

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import {MongooseModule} from "@nestjs/mongoose";
+import { Module } from '@nestjs/common'; 
+import { UsersModule } from './users/users.module';
+
+
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://admDB:cSLhv6j2i5tTJdMD@diamoviedb.t04ue.mongodb.net/M4st3rDB?retryWrites=true&w=majority'),
+    UsersModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
